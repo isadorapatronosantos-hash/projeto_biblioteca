2a7be3d52c01646972112f57a96e7db623348b68
Isadora <isadora.patrono.santos@escola.pr.gov.br>
2026-09-25 07:54:31 -0300

    alteracao

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /tmp/guest-a2zrne/projeto_biblioteca2B/projeto_biblioteca/venv
+set -gx VIRTUAL_ENV /tmp/guest-nqttls/projeto_2B/projeto_biblioteca/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
